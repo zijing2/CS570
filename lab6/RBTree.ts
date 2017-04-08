e6e026e2ca539c6f20ba737471f34d6ae2103589
黄梓敬 <415940392@qq.com>
2017-04-08 17:02:44 -0400

    built a BST structure

---
+++ lab6/RBTree.ts
@@ -0,0 +1,132 @@
+class TreeNode{
+    public left : TreeNode;
+    public right : TreeNode;
+    public val : number;
+
+    constructor(val : number){
+        this. val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+export class RBTree{
+
+    public root;
+
+    /**
+     * insert
+     */
+    public insert(val : number) {
+        var current = this.root;
+        while(current!=null){
+            if(current.val == val){
+                return;
+            }else if(val < current.val){
+                if(current.left == null){
+                    current.left = new TreeNode(val);
+                    return;
+                }else{
+                    current = current.left;
+                }
+            }else{
+                if(current.right == null){
+                    current.right = new TreeNode(val);
+                    return;
+                }else{
+                    current = current.right;
+                }
+            }
+        }
+
+        this.root = new TreeNode(val);
+        return;
+    }
+
+    /** 
+     * delete
+     */
+    public delete(val : number){
+        var current = this.root;
+        var parent = null;
+        var direction;
+        while(current!=null){
+            if(current.val == val){
+                //Node X is leaf
+                if(current.left == null && current.right == null){
+                    parent[direction] = null;
+                    return;
+                }else if((current.left==null && current.right!=null) || (current.left!=null && current.right==null)){
+                    //Node X at most one child
+                    if(current == this.root){
+                        if(current.left!=null){
+                            this.root = current.left;
+                            return;
+                        }else{
+                            this.root = current.right;
+                            return;
+                        }
+                    }else{
+                        if(current.left!=null){
+                            parent[direction] = current.left;
+                            return;
+                        }else{
+                            parent[direction] = current.right;
+                            return;
+                        }
+                    }
+                }else{
+                    //Node X has both, the left and right children
+                    //use method 1: pull the largest element of left Node to current
+                    var c = current.left;
+                    if(c.right == null){
+                        c.right = current.right;
+                        parent[direction] = c;
+                        return;
+                    }
+                    var parent_of_c = null;
+                    while(c.right != null){
+                        parent_of_c = c;
+                        c = c.right;
+                    }
+                    parent_of_c.right = null;
+                    c.left = current.left;
+                    c.right = current.right;
+                    if(current != this.root){
+                        parent[direction] = c;
+                    }else{
+                        this.root = c;
+                    }
+                    return;
+                }
+            }else if (val < current.val){
+                parent = current;
+                direction = "left";
+                current = current.left;
+            }else{
+                parent = current;
+                direction = "right";
+                current = current.right;
+            }
+        }
+        return;
+    }
+
+    /** 
+     * search
+     */
+    public search(val : number){
+        var current = this.root;
+        while(current!=null){
+            if(current.val == val){
+                return true;
+            }else if(val < current.val){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return false;
+    }
+}
